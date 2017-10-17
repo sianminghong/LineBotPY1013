cd6b6b0446a64aa76476a0e4a7c9495a997d5481
sianminghong <32257463+sianminghong@users.noreply.github.com>
2017-10-17 14:24:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('zmWBpCj5sgkK2gyoRKV0Cyk7cQaLwZAwkKx9koVGjEDriKM10LEdmd478Uz3Qquf18Dn6DoPKWCWbMlm2d2/OAiWu2ZGNFWm2zoiWJWvTyz73gkCxs6FIEf5ncvc6drEe/8ATJrhuO5v+aPapjojZwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('129e226166e73165e344323b7836868c')
 
-@app.route("https://linebotpy1013.herokuapp.com:443/callback", methods=['POST'])
+@app.route("https://linebotpy1013.herokuapp.com/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
